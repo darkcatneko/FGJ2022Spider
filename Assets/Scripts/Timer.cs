738e7d0fb185824da395d5a3ca850ca2370520ad
darkcatneko <darkcat7126@gmail.com>
2022-09-03 21:15:17 +0800

    save point

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -5,18 +5,39 @@ using UnityEngine;
 public class Timer : MonoBehaviour
 {
     public float Last_Time = 60f;
+    public float Ant_time;
+    public float Caterpillar_Time;
+    public float Aphid_Time;
+    public float Ant_Time;
     void Start()
     {
         
     }
 
     void Update()
-    {
-        
+    {        
+        if (Last_Time>0)
+        {
+            Last_Time -= Time.deltaTime;
+        }
+        else
+        {
+            Debug.Log("§A¦n»¹");
+        }
     }
-    public void AddTime()
+    public void AddTime(Insect insect)
     {
-
+        switch((int)insect)
+        {
+            case 0:                
+                return;
+            case 1:
+                return;
+            case 2:
+                return;
+            case 3:
+                return;
+        }
     }
 }
 public enum Insect
