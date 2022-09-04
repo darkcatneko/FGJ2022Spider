8b619e8e61e4ebef53206545f9010d236cec8aa6
darkcatneko <darkcat7126@gmail.com>
2022-09-04 14:18:34 +0800

    Fixed

--- Assets/Scripts/PreyTrigger.cs
+++ Assets/Scripts/PreyTrigger.cs
@@ -12,6 +12,7 @@ public class PreyTrigger : MonoBehaviour
         {
             Timer.instance.AddTime(This_insect);
             PlayerData.instance.AddCount(This_insect);
+            Destroy(this.gameObject);
             Instantiate(PlayerData.instance.test,transform.position,Quaternion.identity);
         }
     }
